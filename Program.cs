6415abad33f831c164c2217dd08bbeb3203e0cd6
Henry Andrade <henry.andrade@outlook.com>
2020-11-09 15:14:59 -0300

    Continuacao

--- Program.cs
+++ Program.cs
@@ -6,10 +6,14 @@ namespace Workers
     {
         static void Main(string[] args)
         {
-          
+
+            Console.Write("Enter departament name: ");
+            string deptName = Console.ReadLine();
+
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
             
-            
-            
+
         }
     }
 }
