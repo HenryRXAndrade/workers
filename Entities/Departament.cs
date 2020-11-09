2465c5e5ca0bca9d416064cc885d4eee951b90d5
Henry Andrade <henry.andrade@outlook.com>
2020-11-09 15:06:50 -0300

    Estrutura basica

---
+++ Entities/Departament.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workers.Entities
+{
+    class Departament
+    {
+
+        public string Name { get; set; }
+
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+
+
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workers.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
